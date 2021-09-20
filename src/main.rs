7bba9c863cbe763c602510386a7e50b495af4b90
Alvaro <alvaro11318@gmail.com>
2021-09-20 00:37:33 -0300

    Added keyboard input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,39 @@
 use tetris::*;
+use std::time::Duration;
+use crossterm::event::{poll, read, Event};
+use crossterm::terminal::enable_raw_mode;
+use crossterm::event::{KeyCode, KeyModifiers};
+use crossterm::event::KeyEvent;
+
+fn print_events() -> crossterm::Result<()> {
+    loop {
+        // `poll()` waits for an `Event` for a given time period
+        if poll(Duration::from_millis(500))? {
+            // It's guaranteed that the `read()` won't block when the `poll()`
+            // function returns `true`
+            match read()? {
+                Event::Key(event) => {
+                    if event.code == KeyCode::Char('z') {
+                        println!("Apertou z");
+                    } else if event.code == KeyCode::Char('q') {
+                        std::process::abort();
+                    } else if event.code == KeyCode::Char('c') && event.modifiers == KeyModifiers::CONTROL {
+                        std::process::abort();
+                    }
+                },
+                Event::Mouse(event) => println!("{:?}", event),
+                Event::Resize(width, height) => println!("New size {}x{}", width, height),
+            }
+        } else {
+            // Timeout expired and no `Event` is available
+        }
+    }
+    Ok(())
+}
 
 fn main() {
+    enable_raw_mode().unwrap();
+    print_events().unwrap();
     /*show_tetromino(0);
     show_tetromino(1);
     show_tetromino(2);
@@ -9,14 +42,16 @@ fn main() {
     show_tetromino(5);
     show_tetromino(6);*/
 
-    let mut field = create_field();
+    //let mut field = create_field();
 
-    let mut screen = create_screen();
+    //let mut screen = create_screen();
 
     // Game logic
-    let mut current_piece = 1;
-    let mut current_y = 0;
-    let mut current_x = FIELD_WIDTH/2;
+    //let mut current_piece = 1;
+    //let mut current_y = 0;
+    //let mut current_x = FIELD_WIDTH/2;
+
+
 
     
     
@@ -24,7 +59,7 @@ fn main() {
     
     
     
-    draw_field_on_screen(&field, &mut screen);
+    /*draw_field_on_screen(&field, &mut screen);
 
 
     // Draw tetromino piece on the screen
@@ -37,4 +72,5 @@ fn main() {
 
     //show_field(&field);
     show_screen(&screen);
+    */
 }
